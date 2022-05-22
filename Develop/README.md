aaef1355e570f1d024c74ef17f7d696175fc9f8f
LilCalc <noahsavinois@gmail.com>
2022-05-22 02:01:43 -0500

    Finished index.js, added Questions + function to initialize application. Finished generateMarkdown file, completed table of contents + links to github

---
+++ Develop/README.md
@@ -0,0 +1,13 @@
+# README
+   ![badge](https://github.com/LilCalc/README-generator-)
+   <br> 
+   ![badge](https://img.shields.io/github/languages/count/LilCalc/README-generator-)
+   <br>
+   ![badge](https://img.shields.io/github/issues/LilCalc/README-generator-)
+   <br>
+   ![badge](https://img.shields.io/github/issues-closed/LilCalc/README-generator-)
+   <br>
+   ![badge](https://img.shields.io/github/last-commit/LilCalc/README-generator-)
+   <br>
+   ![badge](https://img.shields.io/badge/license-AAL-important)
+  
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,56 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-`;
-}
-//hello
+
+   return `# ${data.title}
+   ![badge](https://github.com/${data.github}/${data.repo})
+   <br> 
+   ![badge](https://img.shields.io/github/languages/count/${data.github}/${data.repo})
+   <br>
+   ![badge](https://img.shields.io/github/issues/${data.github}/${data.repo})
+   <br>
+   ![badge](https://img.shields.io/github/issues-closed/${data.github}/${data.repo})
+   <br>
+   ![badge](https://img.shields.io/github/last-commit/${data.github}/${data.repo})
+   <br>
+   ![badge](https://img.shields.io/badge/license-${data.license}-important)
+   
+   ## Description
+   
+    ${data.description}
+  
+   ## Table of Contents
+   - [Description](#description)
+   - [Documentation](#documentation)
+   - [Usage](#usage)
+   - [Screenshot](#screenshot)
+   - [Features](#features)
+   - [Acknowledgements](#acknowledgements)
+   - [License](#license)
+   - [Testing](#testing)
+   - [Contact](#contact)
+
+   ## Documentation
+   ${data.documentation}
+  
+   ## Usage
+   ${data.usage}
+   ## Screenshot
+   ![Screenshot](${data.screenshot})
+   ## Features
+   ${data.features}
+   
+   # Acknowledgements
+   ${data.acknowledgements}
+     
+   ## License
+   ![badge](https://img.shields.io/badge/license-${data.license}-important)
+   <br>
+   Permission to use this application is granted under the ${data.license} license. <https://opensource.org/licenses/${data.license}>
+   ## Testing
+   ${data.testing}
+
+   ##Contact:
+   Contact me at <a href="malito:${data.email}">${data.email}</a>`
+  
+ }
+ 
 module.exports = generateMarkdown;
